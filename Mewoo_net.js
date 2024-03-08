986d1fb51b8cee6b4f5aa74178653290dc3fd023
Mewoo-net <162759772+Mewoo-net@users.noreply.github.com>
2024-03-08 18:16:59 -0500

    Delete Mewoo_net.js

--- Mewoo_net.js
+++
@@ -1,9 +0,0 @@
-
-
-var Name
-document.getElementById("button1").onclick = function(){
-    var Name = document.getElementById("text1").value
-    console.log("Hello", Name)
-    document.getElementById("form_lable").innerHTML = `Welcome ${Name}`;
-}
-
